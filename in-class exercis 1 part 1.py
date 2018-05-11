7ae8ca8ac450438db156dc6ec27dc6ba1b2b0abf
pdswift <38026357+pdswift@users.noreply.github.com>
2018-05-10 20:12:15 -0700

    Add files via upload

---
+++ in-class exercis 1 part 1.py
@@ -0,0 +1,9 @@
+# asks th user for their first and last name then prints a uername to the screen
+# CNA 336, spring 18
+# parker, pdswift@student.rtc.edu
+
+first_name = (input('enter your fist name:'))
+last_name = (input('enter your last name:'))
+username = first_name + last_name
+print (username)
+
---
+++ in-class exercise 1.5.py
@@ -0,0 +1,8 @@
+#prints username in lowercase
+#CNA 336, spring 18
+#parker, pdswift@student.rtc.edu
+
+first_name = (input('enter your fist name:'))
+last_name = (input('enter your last name:'))
+username = first_name + last_name
+print (username.lower())
